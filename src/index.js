dd601491bc6bb7b94423774216ea8fa070b772f3
Amit Pandey <amit.pandey@ivvy.com>
2018-08-07 21:13:52 +0530

    - Updated latest packages for node modules - Added changes to App componenet to render the same as class

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDom, {Component} from 'react-dom';
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
 
 const API_KEY = 'AIzaSyB0cutdUnYGWCnsrPPzWH9j3FypXbJNOV4';
 
-// This is a component which will produce HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {'videos': []};
+
+    YTSearch({key:API_KEY, 'term':'surf'}, (videos) => {
+      this.setState(videos)
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
-// Get the generated HTML of component and render it to the page in to the DOM
+
 ReactDom.render(<App />, document.querySelector('.container'));
